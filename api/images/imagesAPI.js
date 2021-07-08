2c176f38566bef3afe1798215b46fd8fb648efab
Daniel Janocha <djanocha@luxbit.com.pl>
2021-07-08 12:47:53 +0200

    added simple frontend

--- api/images/imagesAPI.js
+++ api/images/imagesAPI.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const multer = require('multer')
+const fs = require('fs')
+const sharp = require('sharp')
 
+const inputFolder = path.join(__dirname, "..", '..', 'upload')
+const outputFolder = path.join(__dirname, '..', '..', 'results')
 
 //multer configs:
 const storageStrategy = multer.diskStorage({
@@ -10,41 +14,71 @@ const storageStrategy = multer.diskStorage({
         cb(null, 'upload/')
     },
     filename: (req, file, cb) => {
-        if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg')
-            cb(null, file.originalname)
-        else cb(null, false)
+        cb(null, 'original.png')
     }
 })
 
-// const fileFilterStrategy = (req, file, cb) => {
-//     if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') cb(null, true)
-//     else cb(null, false);
-// }
+const fileFilterStrategy = (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpeg') cb(null, true)
+    else cb(null, false);
+}
 
-const upload = multer({ dest: 'upload', storage: storageStrategy })
-
-
-
-
-
-router.get('/', (req, res) => {
-    res.status(200).json({ message: "you received all the images" })
+const upload = multer({
+    dest: 'upload/',
+    storage: storageStrategy,
+    fileFilter: fileFilterStrategy,
 })
-router.get('/:id', (req, res) => {
-    const id = req.params.id; // it's still a string, not a number
-    if (id === "100" || id === "200" || id === "400") {
-        res.status(200).json({ message: `you received image of ${id}px width` })
+
+
+//routes
+
+router.post('/images', upload.single('originalImage'), async(req, res) => {
+        const widths = [100, 200, 400];
+        console.log(req.file);
+
+        for (width of widths) {
+            await sharp(req.file.path).resize(width).toFile(path.join(outputFolder, `${width}.png`))
+
+        }
+        fs.unlinkSync(req.file.path)
+            // res.status(200).json({ message: "you posted an image" })
+        res.sendFile(path.join(__dirname, '..', '..', 'public', 'download.html'))
+
+    })
+    // router.get('/download', async(req,res)=>{
+    //     res.sendFile(path.join(__dirname, '..','..','public','download.html'))
+    // })
+router.post('/download/:name', async(req, res) => {
+    const name = req.params.name;
+    const fullname = path.join(outputFolder, name);
+    res.send(name)
+    if (name === '100' || name === '200' || name === '400') {
+        res.send(fullname)
     } else {
-        res.status(404).json({ message: "width not found" })
+        res.status(500).json({ message: 'could not find given file' })
     }
-})
-router.post('/', upload.single('originalImage'), (req, res) => {
-    console.log(req.file);
-
-    res.status(200).json({ message: "you posted an image" })
 
 })
+
 router.get('/', (req, res) => {
+    ///////////////////////////////////////////////////////////////////
+    //when coming to the new page, remove all the files uploaded before:
+    //doesn't work yet :/
+    fs.readdirSync(outputFolder, (err, files) => {
+        if (err) {
+            console.log('-------------------errr---------------------')
+            throw err;
+        }
+        for (file of files) {
+            console.log('-----------------------------')
+            console.log(file)
+            fs.unlink(path.join(outputFolder, file), error => {
+                if (error) throw error;
+            })
+        }
+    });
+    ///////////////////////////////////////////////
+    // console.log(__dirname);
     res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 })
 
--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express')
 const imagesAPI = require('./api/images/imagesAPI')
 const pagesAPI = require('./api/pages/pagesAPI')
 const app = express();
+
 // const path = require('path')
+
+app.use(express.static('public'));
+
 
 //let's add route responsible for sending html page with input form; 
 app.use('/', pagesAPI)
 //use the router responsible for handling imageAPI requests(GET, POST) as one of the middlewares
 //do it with redirecting every host:port/images request to that api:
-app.use('/images',imagesAPI);
+// app.use('/images',imagesAPI);
+app.use('/', imagesAPI)
+
+
 
 
 
@@ -32,9 +39,11 @@ app.use((req,res, next)=>{
 
 //handle any incoming errors including ours (created right above):
 
-app.use((error, req ,res,next)=>{
-    res.status(error.status || 500).json({
-        error,
-    })
-})
+// app.use((error, req ,res,next)=>{
+//     res.status(error.status || 500).json({
+//         error,
+//     })
+// })
+
+
 module.exports=app;
